c5ab82fda27c72f9d361ca4ba658fb77babaec07
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-18 01:13:24 +0100

    create category class

---
+++ lib/models/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color color;
+
+  Category({
+    @required this.id,
+    @required this.title,
+    @required this.color,
+  });
+}
