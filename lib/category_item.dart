f8efd1bb694717bffadc91de00b0b75770fdf546
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-18 02:40:53 +0100

    change screens

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,10 +7,17 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: ((_) => CategoryMealsScreen())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(14),
       child: Container(
         padding: const EdgeInsets.all(14),
         decoration: BoxDecoration(
