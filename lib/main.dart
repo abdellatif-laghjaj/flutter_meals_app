9ec568dc9b277081dc48b818682e7b134f82c4c4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-18 01:42:13 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
-    );
-  }
-}
