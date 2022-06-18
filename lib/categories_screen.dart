e926e66824e62ede0905cdfb335614fa44102521
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-18 01:02:07 +0100

    categories scree

---
+++ lib/categories_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
