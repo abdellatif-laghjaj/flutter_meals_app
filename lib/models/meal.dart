c427bdad4b7f022c2c41e3ed6144521532fad790
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-19 01:47:00 +0100

    update meal class

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-enum Complexity { simple, challenging, hard }
+enum Complexity { Simple, Challenging, Hard }
+
+enum Affordability { Affordable, Pricey, Luxurious }
 
 class Meal {
   final String id;
@@ -10,6 +12,11 @@ class Meal {
   final List<String> steps;
   final int duration;
   final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
 
   Meal({
     @required this.id,
@@ -19,5 +26,10 @@ class Meal {
     @required this.steps,
     @required this.duration,
     @required this.complexity,
+    @required this.affordability,
+    @required this.isGlutenFree,
+    @required this.isLactoseFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
   });
 }
