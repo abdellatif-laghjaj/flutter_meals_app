1b5435d1250deb7a8235bb531ce56beaab804ab4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-22 00:56:44 +0100

    create tabs

---
+++ lib/screens/tabs_scree.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {}
+}
