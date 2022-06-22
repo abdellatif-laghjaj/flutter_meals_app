360941efe5eb6726b653312108850215d70e78df
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-22 01:04:52 +0100

    add tab bar

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {}
+}
--- lib/screens/tabs_scree.dart
+++ lib/screens/tabs_scree.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'categories_screen.dart';
+import 'favorites_screen.dart';
+
 class TabsScreen extends StatefulWidget {
   @override
   State<TabsScreen> createState() => _TabsScreenState();
@@ -7,5 +10,32 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals App'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
 }
