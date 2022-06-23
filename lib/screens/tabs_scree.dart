7efa743052337ec67b521c9328a65d0e6791acc5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-23 01:29:39 +0100

    remove top bar

--- lib/screens/tabs_scree.dart
+++ lib/screens/tabs_scree.dart
@@ -11,32 +11,11 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals App'),
-          bottom: TabBar(
-            labelColor: Colors.white,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category, color: Colors.white),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star, color: Colors.white),
-                text: 'Favorites',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals App'),
       ),
+      body: null,
     );
   }
 }
