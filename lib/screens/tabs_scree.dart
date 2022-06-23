aeb5b579b870fa43832519b63d0485e881ae408b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-23 01:44:05 +0100

    add bottom bar

--- lib/screens/tabs_scree.dart
+++ lib/screens/tabs_scree.dart
@@ -9,13 +9,44 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Meals App'),
       ),
-      body: null,
+      body: _pages[_selectPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+            tooltip: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+            tooltip: 'Favorites',
+          ),
+        ],
+      ),
     );
   }
 }
