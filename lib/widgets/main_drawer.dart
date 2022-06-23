0fdc389a6208b877b5dca6dea53b398ef2ebbaf6
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-23 02:32:32 +0100

    uodate drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -2,5 +2,30 @@ import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            color: Theme.of(context).primaryColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
 }
